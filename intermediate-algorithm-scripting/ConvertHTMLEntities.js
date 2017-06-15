2b51d4dd06c56e05bf6b9e81265fbe2decc0f205
Zehra Punjwani <punjwanizee@gmail.com>
2017-06-15 15:45:08 +0100

    Intermediate Scripting and Disclaimer update

---
+++ intermediate-algorithm-scripting/ConvertHTMLEntities.js
@@ -0,0 +1,27 @@
+/*
+  Written By: Zehra Punjwani
+  Date: May 2017
+  Details: Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  htmlEntities = {
+    '&':'&amp;',
+    '<':'&lt;',
+    '>':'&gt;',
+    '\"':'&quot;',
+    '\'':"&apos;"
+  };
+  //Use map function to return a filtered str with all entities changed automatically.
+  return str.split('').map(function(entity){
+    return htmlEntities[entity] || entity;
+  }).join('');
+}
+
+// OR
+function convertHTML(str) {
+  return str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,"&apos;");
+}
+
+convertHTML("Dolce & Gabbana");
---
+++ intermediate-algorithm-scripting/SpinalTapCase.js
@@ -0,0 +1,16 @@
+/*
+	Written By: Zehra Punjwani
+	Date: May 2017
+	Details: Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+*/
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  
+  // Replace low-upper case to low-space-uppercase
+  // Split on whitespace and underscores and join with dash
+  return str.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase().split(/(?:_| )+/) .join('-');
+}
+
+spinalCase("This Is Spinal Tap");
