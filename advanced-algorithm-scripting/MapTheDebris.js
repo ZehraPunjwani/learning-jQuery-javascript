c50fa675487dd6ebf0a6beabd7bd26400c1cb9d7
Zehra Punjwani <punjwanizee@gmail.com>
2017-06-19 20:50:15 +0100

    Advanced Algorthm Scripting Finished

---
+++ advanced-algorithm-scripting/MapTheDebris.js
@@ -0,0 +1,26 @@
+/*
+  Written By: Zehra Punjwani
+  Date: June 2017
+  Details: Return a new array that transforms the element's average altitude into their orbital periods.
+  The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+  You can read about orbital periods on wikipedia.
+  The values should be rounded to the nearest whole number. The body being orbited is Earth.
+  The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+*/
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  // Loop through each item in the array arr
+  arr.forEach(function(item) {
+    // Calculate the Orbital period value
+    var tmp = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM));
+    //Delete the avgAlt property
+    delete item.avgAlt;
+    //Add orbitalPeriod property
+    item.orbitalPeriod = tmp;
+  });
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
--- advanced-algorithm-scripting/ValidateUSTelephoneNumbers.js
+++ advanced-algorithm-scripting/ValidateUSTelephoneNumbers.js
@@ -17,7 +17,7 @@
 
 function telephoneCheck(str) {
   // Good luck!
-  return true;
+  return /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/.test(str);
 }
 
 telephoneCheck("555-555-5555");
